035c3f22ecb4dcdc123414d55424f918f208214e
Shaliya56 <161951978+Shaliya56@users.noreply.github.com>
2024-12-26 22:12:51 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const BOT_NUMBER = "94761167172"
+const SESSION_ID = "PRABATH-MD~hqUSXApY#bl3qdYiQjrICbdNKypkTJ4yWbSENKH9dsDUSxEKEJxQ"
 
 
 module.exports = {
